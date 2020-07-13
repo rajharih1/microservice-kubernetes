b10325380567e616e229d7f4cef4dfae2d9fd31e
Rajesh Hariharan <rahariharan@deloitte.ca>
2020-07-13 14:02:16 -0400

    change account

--- microservice-kubernetes-demo/docker-build.sh
+++ microservice-kubernetes-demo/docker-build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 if [ -z "$DOCKER_ACCOUNT" ]; then
-    DOCKER_ACCOUNT=ewolff
+    DOCKER_ACCOUNT=hubrajesh
 fi;
 docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
+docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:1.0.0
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:1.0.0
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
+docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:1.0.0
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:1.0.0
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
+docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:1.0.0
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:1.0.0
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
+docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:1.0.0
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:1.0.0
